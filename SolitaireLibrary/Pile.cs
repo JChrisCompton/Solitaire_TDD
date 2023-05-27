b982aae84625e0e5b362fab70fa2b719935c07b8
J. Chris Compton <JCCompton@gmail.com>
2023-05-27 17:30:26 -0400

    StockToTalon_Move1_CheckRemoval

--- SolitaireLibrary/Pile.cs
+++ SolitaireLibrary/Pile.cs
@@ -1,8 +1,13 @@
-﻿namespace SolitaireLibrary
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SolitaireLibrary
 {
     public class Pile : iPile, iMove
     {
         private List<Card> InProgressCardList { get; set; } = new List<Card>();
+        //private Pile InProgressCardList = new Pile();
 
         //list of cards
         public List<Card> CardList { get; set; } = new List<Card>();
@@ -26,11 +31,114 @@
             return false;
         }
 
-        public bool MoveTo(Pile sourcePile, Pile movePile)
-        {
-            throw new NotImplementedException();
+        public bool MoveTo(Pile toPile, int startCardLoc = 0)
+        {   // **************************************************************************************************
+            // Okay... I remember there's a way to do a type(T) or something like, that which is more OO - but Ken, I'm on a role so that is a todo for the moment
+            //
+            //TODO: Do this the object oriented way instead of using temporary strings 
+            //  there may also be a way to assign the name of StockPile as something else like "ThisIsThe_StockPile" not sure why I'd want to do that but for somereason it hit me just now.  Squirrel!!!  Stupid rabbit hole.
+            // **************************************************************************************************
+
+            //return false;
+            //throw new NotImplementedException();
+            //if (pile == null) return false;
+
+            bool ret = true;    // initialize as no errors
+
+            // determine where we are moving from
+            var fromPile = (Pile)this;
+            var fromPileType = this.GetType().Name.ToString();
+            var destPileType = toPile.GetType().Name.ToString();
+            Debug.Print($"* fromPile's type == " + fromPileType + ".");
+            Debug.Print($"*  toPile's type  == " + destPileType + ".");
+
+            // Ken - I assume it is best to now call the private specialized proc depending on what is being moved where... b/c otherwise this proc becomes RATHER long.
+
+            if (ret == true)
+            {   // no errors yet
+                ret = MoveStockToTalon((StockPile)fromPile, (TalonPile)toPile, 0);
+            }
+
+            return ret;
+
         }
 
+        private bool MoveStockToTalon(StockPile stock, TalonPile talon, int startCardLoc)
+        {
+            bool ret = true;    // initialize as no errors
+
+            if (true) // Output param values
+            {
+                //DEBUG: print out the pile values
+                Debug.Print("");
+                Debug.Print("============= Initial Values =======================");
+                Debug.Print("stock - \n | " + GetPileContents((Pile)stock));
+                Debug.Print("talon - " + GetPileContents((Pile)talon));
+                Debug.Print($"startCardLoc == {startCardLoc.ToString()}");
+                Debug.Print("InProgressCardList - size == " + InProgressCardList.Count.ToString());
+            }
+            #region VerifyParameters
+            // verify parameters
+            if (InProgressCardList == null) return false;
+            if (InProgressCardList.ToList().Count >= 1 ) return false;    //.cardList.Count() > 0
+            if (startCardLoc < 0) return false;
+            if (stock.CardList.Count <= startCardLoc) return false;     // .Count minus 1 ?
+            if (startCardLoc > 13) return false;
+            if (stock == null) return false;
+            if (stock.CardList.Count < 1) return false;
+            if (stock.CardList.Count >52) return false;
+            if (talon == null) return false;
+            if (talon.CardList.Count < 1) return false;
+            if (talon.CardList.Count > 52) return false;
+            // Verify these again as I had to use Pile for the parameter types (a more generic type) 
+            if (stock.GetType().Name.ToString().ToLower() != (new StockPile()).GetType().Name.ToString().ToLower()) return false;
+            if (talon.GetType().Name.ToString().ToLower() != (new TalonPile()).GetType().Name.ToString().ToLower()) return false;
+            #endregion
+            // TODO: for now assume startCardLoc == zero (1 card, the top one)
+
+            // Remove the card from the stock pile
+            // Store card in temp location
+            //InProgressCardList.CardList.AddCard(stock.CardList[startCardLoc]);
+            Card c = stock.CardList[startCardLoc];
+            c.isFaceUp = true;  // flip the card before adding it to the pile
+            InProgressCardList.Add(c);
+            //InProgressCardList.CardList.Count should == 1
+            stock.CardList.Remove(c);
+            //            talon.CardList.Remove[startCardLoc]; why does this error?
+
+            // add card to destination
+            //talon.CardList.Add(InProgressCardList.Add(stock.CardList[startCardLoc]));
+            talon.CardList.Insert(0, c);
+
+            if (true) // Output param values
+            {
+                //DEBUG: print out the pile values
+                Debug.Print("");
+                Debug.Print("============= Before returning =====================");
+                Debug.Print("»stock " + GetPileContents((Pile)stock));
+                Debug.Print("»talon " + GetPileContents((Pile)talon));
+                Debug.Print($"startCardLoc == {startCardLoc.ToString()}");
+                Debug.Print("InProgressCardList - size == " + InProgressCardList.Count.ToString());
+            }
+            return ret;
+        }
+
+        private string GetPileContents(Pile pile)
+        {
+            // yeah not string builder, but I'm debugging
+            string ret = $"Pile name == {pile.GetType().Name} size == {pile.CardList.Count.ToString()}:  ";
+            if (pile == null)
+            {
+                ret = pile.GetType().Name.ToString();
+                return $"Pile  {ret}  has no cards!";
+            }
+                
+            foreach (Card c in pile.CardList) { 
+                if (c == null) continue;
+                ret += $"{c.Value.ToString()}-{c.Suit} {c.isFaceUp.ToString()}, ";
+            }
+            return ret;
+        }
         //public void Undo(Pile movePile)
         //{
         //    throw new NotImplementedException();
--- SolitaireTests/PileTests.cs
+++ SolitaireTests/PileTests.cs
@@ -2,6 +2,7 @@
 using SolitaireLibrary;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -39,6 +40,57 @@ namespace SolitaireTests
             var testPile = new Pile();
             testPile.CardList.Add(new Card(3, SuitType.Diamond, true));
             Assert.AreEqual(testPile.CardList.Count(), 1);
+        }
+        [TestMethod]
+        public void StockToTalon_MoveOne_CheckRemoval()
+        {
+            Debug.Print("\n\n\n");
+            // When moving from Stock to Talon check that a card is removed from Stock and added to Talon
+            var stockPile = new StockPile();
+            var talonPile = new TalonPile();
+
+            stockPile.CardList.Add(new Card(2, SuitType.Spade, false));
+            stockPile.CardList.Add(new Card(3, SuitType.Spade, false));
+            stockPile.CardList.Add(new Card(4, SuitType.Spade, false));
+            talonPile.CardList.Add(new Card(5, SuitType.Diamond, true));
+            talonPile.CardList.Add(new Card(6, SuitType.Diamond, true));
+            talonPile.CardList.Add(new Card(7, SuitType.Diamond, true));
+
+            int stockPileHeightBefore = stockPile.CardList.Count();
+            int talonPileHeightBefore = stockPile.CardList.Count();
+
+            // Make the move
+            bool isSuccess = stockPile.MoveTo(talonPile);
+            
+            // was there an error, or success?
+            Assert.IsTrue(isSuccess);
+            Assert.AreEqual(stockPileHeightBefore - 1, stockPile.CardList.Count());
+            Assert.AreEqual(talonPileHeightBefore + 1, talonPile.CardList.Count());
+        }
+        [TestMethod]
+        public void StockToTalon_MoveOne_CheckDestination()
+        {
+            // When moving from Stock to Talon check that the correct card is removed
+            var stockPile = new StockPile();
+            var talonPile = new TalonPile();
+
+            stockPile.CardList.Add(new Card(2, SuitType.Spade, true));
+            stockPile.CardList.Add(new Card(3, SuitType.Spade, true));
+            stockPile.CardList.Add(new Card(4, SuitType.Spade, true));
+            talonPile.CardList.Add(new Card(5, SuitType.Diamond, true));
+            talonPile.CardList.Add(new Card(6, SuitType.Diamond, true));
+            talonPile.CardList.Add(new Card(7, SuitType.Diamond, true));
+
+            int pileHeightBefore = stockPile.CardList.Count();
+
+            // Make the move
+            bool isSuccess = stockPile.MoveTo(talonPile);
+
+            //// was there an error, or success?
+            //Assert.IsTrue(isSuccess);
+            //Assert.AreEqual(pileHeightBefore - 1, stockPile.CardList.Count());
+
+            Assert.AreEqual(1,2);
         }
     }
 }
