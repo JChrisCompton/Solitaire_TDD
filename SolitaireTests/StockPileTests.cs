582a44d363960daa045387648aca5281bd175da3
DuncDan <kmwebb@cinci.rr.com>
2023-06-19 17:25:55 -0400

    Added Talon MoveTo tests and code
    Not complete but good for next discussion

--- SolitaireTests/StockPileTests.cs
+++ SolitaireTests/StockPileTests.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using SolitaireLibrary;
 using System.Diagnostics;
-using System.Runtime.CompilerServices;
+//using System.Runtime.CompilerServices;
 
 namespace SolitaireTests
 {
